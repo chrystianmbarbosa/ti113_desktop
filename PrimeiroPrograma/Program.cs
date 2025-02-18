2b308e36788f9fd855a317fe2282a1b2ab1bb18a
chrystianmbarbosa <chrystian.mbarbosa@senacsp.edu.br>
2025-02-18 20:00:52 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroPrograma/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PrimeiroPrograma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe seu Nome: ");
+            string nome = Console.ReadLine()!;
+            Console.WriteLine("\n");
+            Console.Write("Informe a Nota 1: ");
+            //float nota1 = Convert.ToSingle(Console.ReadLine()!);
+            float nota1 = float.Parse(Console.ReadLine()!);
+            Console.WriteLine("\n");
+            Console.Write("Informe a Nota 2: ");
+            float nota2 = float.Parse(Console.ReadLine()!);
+            Console.WriteLine("\n");
+            Console.Write("Informe a Nota 3: ");
+            float nota3 = float.Parse(Console.ReadLine()!);
+
+            float soma = nota1 + nota2 + nota3;
+            float media = soma / 3;
+
+            Console.WriteLine($"A média de {nome} é {media}");
+          
+        }
+    }
+}
---
+++ StatusAluno/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace StatusAluno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Faça um algoritmo para a leitura de duas notas parciais 
+             * de um aluno.
+             * O algoritmo deve calcular a média alcançada por aluno 
+             * e apresentar:
+            
+                A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
+                A mensagem "Reprovado", se a média for menor do que cinco;
+                A mensagem “Recuperação", se a média estiver entre cinco e sete.
+            */
+            double nota1 = 0;
+            double nota2 = 0;
+            string mensagem = "";
+            double media = 0;
+
+            Console.Write("Informe a 1ª nota: ");
+            nota1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\n");
+            Console.Write("Informe a 2ª nota: ");
+            nota2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\n");
+            media = (nota1 + nota2) / 2;
+
+            if (media >= 7)
+            {
+                mensagem = "Aprovado";
+            }
+            else if (media >= 5 && media < 7)
+            {
+                mensagem = "Recuperação";
+            }
+            else
+            {
+                mensagem = "Reprovado";
+            }
+            Console.WriteLine($"O Status do Aluno de Média: {media} é {mensagem}");
+
+        }
+    }
+}
