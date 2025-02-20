c2915b930bc2c28f64b11d2fb4ff41d10662445d
chrystianmbarbosa <chrystian.mbarbosa@senacsp.edu.br>
2025-02-19 22:05:07 -0300

    feat: Exercício 10 Corrigido

--- Exercicio10/Program.cs
+++ Exercicio10/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool sair = false;
+            do
+            {
+                Console.Clear();
+                Console.Write("Informe o valor da Tabuada: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 1; i <= 10; i++)
+                    Console.WriteLine($"{num} X {i} = {num * i}");
+
+                Console.WriteLine("Você deseja sair? S = Sim / N = Não ");
+                string op = Console.ReadLine()!;
+                sair = (op == "S");
+                
+            }
+            while (sair == false);
+
         }
     }
 }
